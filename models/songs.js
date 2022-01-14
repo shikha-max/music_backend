9f0478f42543cc3ca3cf0d6adddccc6967ac2764
shikha-max <tshikha66@mail.com>
2022-01-14 21:21:48 +0530

    posted songs

--- models/songs.js
+++ models/songs.js
@@ -3,7 +3,11 @@ const mongoose = require('mongoose')
 const songSchema=new mongoose.Schema({
     type:{type:String,required:true,default:"songs"},
     name:{type:String,required:true},
-    album:{type:mongoose.Schema.Types.ObjectId,ref:"album",required:true}
+    album:{type:mongoose.Schema.Types.ObjectId,ref:"album",required:true},
+    img:{type:String,required:true},
+    duration:{type:Number,required:true},
+    monthlyListeners:{type:Number,default:Math.floor((Math.random()*1000000)+1)}
 })
 
 module.exports =mongoose.model('songs',songSchema)
+
