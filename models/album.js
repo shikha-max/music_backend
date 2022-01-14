6af8f229985f4664c19b8a8c8fd386e44f72eb0d
shikha-max <tshikha66@mail.com>
2022-01-14 20:50:28 +0530

    relationship updated

--- models/album.js
+++ models/album.js
@@ -2,11 +2,10 @@ const mongoose = require('mongoose')
 
 const albmSchema=new mongoose.Schema({
     artist:{type:mongoose.Schema.Types.ObjectId,required:true,ref:"user"},
-    album_name:{type:String,required:true},
-    type:{type:String,required:true,default:"album"},
+    type:{type:String,default:"album"},
     genre:{type:String,required:true},
     img:{type:String,required:true},
-    year:{type:String,required:true}
+    year:{type:Number,required:true}
 })
 
 
