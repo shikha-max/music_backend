e186a4a88748e51aacf40cc07d14897ee88172dd
shikha-max <tshikha66@mail.com>
2022-01-14 21:23:32 +0530

    posted songs

--- models/songs.js
+++ models/songs.js
@@ -5,7 +5,7 @@ const songSchema=new mongoose.Schema({
     name:{type:String,required:true},
     album:{type:mongoose.Schema.Types.ObjectId,ref:"album",required:true},
     img:{type:String,required:true},
-    duration:{type:Number,required:true},
+    duration:{type:String,required:true},
     monthlyListeners:{type:Number,default:Math.floor((Math.random()*1000000)+1)}
 })
 
