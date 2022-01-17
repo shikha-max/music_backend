35750e1f8ee4ae3f1619e82f185bbce6e15235bf
shikha-max <tshikha66@mail.com>
2022-01-18 00:43:32 +0530

    filter

--- controllers/albumController.js
+++ controllers/albumController.js
@@ -85,7 +85,7 @@ router.get('/',async (req, res)=>{
     let page= +req.query.page||1
     let limit= +req.query.limit||2
     let offset=Math.ceil((page-1)*limit)
-    let totalpage= await Album.find({genre:s}).countDocuments()
+    let totalpage= await Album.find({genre:{$regex:nikal}}).countDocuments()
     totalpage=Math.ceil(totalpage/limit)
    
     let resp= await Album.find({genre:{$regex:nikal}}).populate('artist').skip(offset).limit(limit).lean().exec()
