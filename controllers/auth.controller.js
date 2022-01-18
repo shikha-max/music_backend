1b6e786c577e797a3efcffb4e74ecf3a440a02ff
shikha-max <tshikha66@mail.com>
2022-01-18 13:43:21 +0530

    login updated

--- controllers/auth.controller.js
+++ controllers/auth.controller.js
@@ -47,8 +47,8 @@ router.post("/login", async (req, res) => {
 
     if (!user)return res.status(404).send({ err: "user not find please sign up" });
 
-    if (user.password == req.body.password)return res.status(200).send({msg: "successfull login",data:user });
-    else return res.status(400).send({msg:'unsuccessfull'})
+    if (user.password == req.body.password) return res.status(200).send({msg: "successfull login",data:user });
+    else return res.status(200).send({msg:'unsuccessfull'})
   
   } catch (error) {
     return res.status(404).send({ err: error });
